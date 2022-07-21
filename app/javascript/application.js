b23d2a34ab9a0010a1439526d3beb16345a90634
Sumio K <tap.miwo@gmail.com>
2022-07-22 07:01:11 +0900

    fix little bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
-import "bootstrap/scss/bootstrap.scss"import * as bootstrap from "bootstrap"
+import "bootstrap/scss/bootstrap.scss"
+import * as bootstrap from "bootstrap"
